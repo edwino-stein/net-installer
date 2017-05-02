c444efa1c9f220b42e1beac207849f1bdd76eebf
Edwino Stein <edwino.stein@gmail.com>
2017-05-02 19:37:38 -0400

    Versão estável 1.0.0

--- NetInstaller/version.h
+++ NetInstaller/version.h
@@ -2,9 +2,9 @@
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
 
-#define VERSION_MAJOR            0
-#define VERSION_MINOR            9
-#define VERSION_REVISION         9
+#define VERSION_MAJOR            1
+#define VERSION_MINOR            0
+#define VERSION_REVISION         0
 #define VERSION_BUILD            0
 
 #define VER_COMPANY_NAME_STR "Edwino Stein"
