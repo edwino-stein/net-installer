398333d48f58a5f51e7fc38c8df26e152ff4f9c7
Edwino Stein <edwino.stein@gmail.com>
2018-07-08 16:01:32 -0400

    Removido antigo projeto do Visual Studio em C++

--- NetInstaller/Scripts/startup.bat
+++
@@ -1,8 +0,0 @@
-@ECHO off
-chcp 1252 >nul 2>&1
-
-echo Por favor, aguarde...
-call "%~dp0\wait.bat" 5000
-
-cd \
-NetInstaller.exe
--- NetInstaller/src/abstract/GroupBox.cpp
+++
@@ -1,7 +0,0 @@
-#include "../../header/abstract/GroupBox.h"
-
-GroupBox::GroupBox() : Label() {
-	this->type = Control::TYPE_GROUPBOX;
-	this->style = Control::BASIC_STYLE | BS_GROUPBOX;
-	this->text = "GROUPBOX";
-}
