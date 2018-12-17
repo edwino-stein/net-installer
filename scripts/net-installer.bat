523aea8a3436bce54ceb18d438436ce679b9a4f8
Edwino Stein <edwino.stein@gmail.com>
2018-12-16 21:30:14 -0400

    Adicionando uma pausa no scritp de inicialização para quando o programa é encerrado de forma inesperada

--- scripts/net-installer.bat
+++ scripts/net-installer.bat
@@ -1,7 +1,14 @@
 @ECHO off
+
 cd \
 set python=Python3\python.exe
 set mainEntry=net-installer\main.py
 
 call %python% %mainEntry%
+
+if errorlevel 1 (
+    echo Ocorreu um erro inesperado e programa foi encerrado. O sistema sera reinicado.
+    pause
+)
+
 call wpeutil reboot
